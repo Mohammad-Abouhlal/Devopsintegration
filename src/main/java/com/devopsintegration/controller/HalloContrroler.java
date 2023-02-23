6e3bd5881e6219e76652c8c840ac8051ef7fbc77
Mohammad-Abouhlal <mhdabouhlal@gmail.com>
2023-02-23 15:21:22 +0100

    Add HalloController

--- src/main/java/com/devopsintegration/controller/HalloContrroler.java
+++ src/main/java/com/devopsintegration/controller/HalloContrroler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HalloContrroler {
     @GetMapping
     public String message(){
+        System.out.println("Hallo");
         return "Welcome Mohammad to DevOps Integration with Jenkins and Docker and Kubernetes";
     }
 }
